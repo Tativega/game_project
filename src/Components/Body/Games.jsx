d2510f1b8df80da0edbad1fba9b823ea07223740
Manu Aceituno <manuel.n.aceituno@gmail.com>
2020-04-07 18:31:12 -0700

    Se cambio la estructuras de carpetas del proyecto y se anhadio un componente para el banner

---
+++ src/Components/Body/Games.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+import { GamesStyled } from "./style";
+import Single from "./Single"
+
+function Games () {
+return(
+    <GamesStyled>
+        <Single/>
+        <Single/>
+        <Single/>
+        <Single/>
+    </GamesStyled>
+    )
+}
+
+export default Games;
--- src/components/Body/SingleView.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { SingleContainer } from "./style";
-
-function SingleView() {
-  return <SingleContainer></SingleContainer>;
-}
-
-export default SingleView;
