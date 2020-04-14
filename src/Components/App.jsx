e2074eae8ac64779b83d4713ff6a82e2e05ac9db
Federico <fariganello@gmail.com>
2020-04-14 14:01:03 -0300

    feature/pongComponent

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 import { Home } from "../Views/home";
+import Pong from "../Components/Body/Pong"
 
 const App = () => {
       return (
             <div>
                   <Route exact path="/" component={Home} />
+                  <Route exact path="/pong" component={Pong} />
             </div>
       );
 };
