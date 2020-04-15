a8c869c667851594fab933d1925ea22db07b7b4d
Federico <fariganello@gmail.com>
2020-04-15 14:05:33 -0300

    la pelota se mueve y rebota con los bordes de la pantalla

--- src/Components/Body/Pong.jsx
+++ src/Components/Body/Pong.jsx
@@ -1,10 +1,10 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useRef} from "react";
 
 const Pong = () => {
 
     const canvasRef = useRef(null);
 
-    const [ball, setBall] = useState({
+    const ball = useRef({
         position: {
             x: 320,
             y: 240,
@@ -16,26 +16,38 @@ const Pong = () => {
         radius: 5, 
     });
 
+    useEffect( () => {
+
+        window.requestAnimationFrame(gameLoop)
+    },[])
+
     const gameLoop = (timestamp) => {
         update();
+        detectCollision();
         draw();
 
         window.requestAnimationFrame(gameLoop)
     }
 
-    useEffect( () => {
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext('2d');
-
-        window.requestAnimationFrame(gameLoop)
-    },[])
-
     const update = () => {
-        setBall(previous => ({
-            ...previous,
-            position: {x: previous.position.x + previous.velocity.x , 
-                    y: previous.position.y + previous.velocity.y}
-        }))
+        const prevBall = ball.current;
+        prevBall.position.x += prevBall.velocity.x;
+        prevBall.position.y += prevBall.velocity.y;
+    }
+
+    const detectCollision = () => {
+        const canvas = canvasRef.current;
+        const prevBall = ball.current;
+
+        if(prevBall.position.y + prevBall.radius === canvas.height || 
+            prevBall.position.y - prevBall.radius === 0){
+            prevBall.velocity.y = -prevBall.velocity.y;
+        }
+
+        if(prevBall.position.x + prevBall.radius === canvas.width || 
+            prevBall.position.x - prevBall.radius === 0){
+            prevBall.velocity.x = -prevBall.velocity.x;
+        }
     }
 
     const draw = () => {
@@ -45,7 +57,7 @@ const Pong = () => {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.fillStyle = 'white';
         ctx.beginPath();
-        ctx.arc(ball.position.x, ball.position.y, ball.radius, 0, 2 * Math.PI);
+        ctx.arc(ball.current.position.x, ball.current.position.y, ball.current.radius, 0, 2 * Math.PI);
         ctx.fill();
     }
 
