ae3aad3e6e5dd2d207f7eb2ee046264712b8376f
Federico <fariganello@gmail.com>
2020-04-16 09:58:41 -0300

    added the segmented middle line and a border to the canvas

--- src/Components/Body/Pong.jsx
+++ src/Components/Body/Pong.jsx
@@ -4,7 +4,7 @@ const Pong = () => {
 
     const canvasRef = useRef(null);
 
-    const ball = useRef({
+    const refBall = useRef({
         position: {
             x: 320,
             y: 240,
@@ -30,40 +30,58 @@ const Pong = () => {
     }
 
     const update = () => {
-        const prevBall = ball.current;
-        prevBall.position.x += prevBall.velocity.x;
-        prevBall.position.y += prevBall.velocity.y;
+        const ball = refBall.current;
+
+        //Update ball position
+        ball.position.x += ball.velocity.x;
+        ball.position.y += ball.velocity.y;
     }
 
     const detectCollision = () => {
         const canvas = canvasRef.current;
-        const prevBall = ball.current;
+        const ball = refBall.current;
 
-        if(prevBall.position.y + prevBall.radius === canvas.height || 
-            prevBall.position.y - prevBall.radius === 0){
-            prevBall.velocity.y = -prevBall.velocity.y;
+        //Top and bottom collision
+        if(ball.position.y + ball.radius === canvas.height || 
+            ball.position.y - ball.radius === 0){
+            ball.velocity.y = -ball.velocity.y;
         }
 
-        if(prevBall.position.x + prevBall.radius === canvas.width || 
-            prevBall.position.x - prevBall.radius === 0){
-            prevBall.velocity.x = -prevBall.velocity.x;
+        //Left and Right wall collision
+        if(ball.position.x + ball.radius === canvas.width || 
+            ball.position.x - ball.radius === 0){
+            ball.velocity.x = -ball.velocity.x;
         }
     }
 
     const draw = () => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
+        const ball = refBall.current;
 
+        //Clear canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        
+
+        //Draw the middle segmented line
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 10;
+        ctx.setLineDash([20, 15]);
+        ctx.beginPath();
+        ctx.moveTo(canvas.width/2, 0);
+        ctx.lineTo(canvas.width/2, canvas.height);
+        ctx.stroke();
+
+        //Draw the ball
         ctx.fillStyle = 'white';
         ctx.beginPath();
-        ctx.arc(ball.current.position.x, ball.current.position.y, ball.current.radius, 0, 2 * Math.PI);
+        ctx.arc(ball.position.x, ball.position.y, ball.radius, 0, 2 * Math.PI);
         ctx.fill();
     }
 
     return(
         <div>
-            <canvas ref={canvasRef} width={640} height={480}/>
+            <canvas ref={canvasRef} width={640} height={480} style={{border:"1px solid white"}}/>
         </div>
     ) 
 }
