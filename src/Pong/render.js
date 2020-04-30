39700f9485ed257b3b4e7d9295f404dcab5ece30
Federico <fariganello@gmail.com>
2020-04-30 09:15:43 -0300

    added render functions and some math explanations

---
+++ src/Pong/render.js
@@ -0,0 +1,37 @@
+import { WINDOW_HEIGHT, WINDOW_WIDTH } from "../Pong/constants";
+
+export const drawBall = (ctx, ball) => {
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.arc(ball.position.x, ball.position.y, ball.radius, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+export const drawGameOver = (ctx, winner) => {
+    ctx.font = "60px Arial";
+    ctx.fillText(winner + " wins!", WINDOW_WIDTH / 6, WINDOW_HEIGHT / 2);
+}
+
+export const drawMiddleLine = (canvas, ctx) => {
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 10;
+    ctx.setLineDash([20, 15]);
+    ctx.beginPath();
+    ctx.moveTo(canvas.width/2, 0);
+    ctx.lineTo(canvas.width/2, canvas.height);
+    ctx.stroke();
+}
+
+export const drawPaddle = (ctx, paddle, x, y) => {
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.rect(x, y, paddle.width, paddle.height);
+    ctx.fill();
+    ctx.closePath();
+}
+
+export const drawScore = (ctx, score) => {
+    ctx.font = "30px Arial";
+    ctx.fillText(score.player1, WINDOW_WIDTH / 2 - 70 , 50);
+    ctx.fillText(score.player2, WINDOW_WIDTH / 2 + 50 , 50);
+}
