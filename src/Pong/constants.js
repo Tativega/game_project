fc26ba5753533cc33be9f2ea5d6c6073e3065dd0
aleaker <alejoakerfeld@gmail.com>
2020-06-08 17:23:23 -0300

    changed constants

--- src/Pong/constants.js
+++ src/Pong/constants.js
@@ -9,7 +9,9 @@ export const PADDLE_PADDING = 50;
 
 //Ball
 export const BALL_SPEED = 4;
-export const SPEED_INCREASE = 0.3;
+export const EASY_SPEED_INCREASE = 0.1;
+export const NORMAL_SPEED_INCREASE = 0.3;
+export const HARD_SPEED_INCREASE = 0.6;
 
 //Settings
 export const CONFIG_BALL_TEXT_POSITION = 0.15;
@@ -18,9 +20,5 @@ export const CONFIG_KEYBOARD_TEXT_POSITION = 0.45;
 export const ONE_OR_TWO_PLAYERS_TEXT_POSITION = 0.60;
 export const BACK_TEXT_POSITION = 0.9;
 export const SPEED_DIFICULTY_TEXT_POSITION = 0.25;
-
-export const EASY_SPEED_INCREASE = 0.1;
-export const NORMAL_SPEED_INCREASE = 0.3;
-export const HARD_SPEED_INCREASE = 0.6;
 
  
